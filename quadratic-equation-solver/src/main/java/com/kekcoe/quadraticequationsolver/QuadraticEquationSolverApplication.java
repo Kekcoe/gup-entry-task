3ba5394a0293cc543241d577298da4dc08d3ceb2
kekcoe <kekcoe@gmail.com>
2023-11-06 15:06:34 +0300

    initial commit

---
+++ quadratic-equation-solver/src/main/java/com/kekcoe/quadraticequationsolver/QuadraticEquationSolverApplication.java
@@ -0,0 +1,13 @@
+package com.kekcoe.quadraticequationsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadraticEquationSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadraticEquationSolverApplication.class, args);
+	}
+
+}
---
+++ quadratic-equation-solver/src/test/java/com/kekcoe/quadraticequationsolver/QuadraticEquationSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kekcoe.quadraticequationsolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadraticEquationSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
